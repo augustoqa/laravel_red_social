02c584859113792e0aa0742c81fd0ce31a83c97f
César Acual <augustoacual@gmail.com>
2021-04-07 10:47:13 -0600

    Diseñando la plantilla con Bootstrap 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
-<form action="{{ route('statuses.store') }}" method="POST">
-    @csrf
-    <textarea name="body" id="" cols="30" rows="10"></textarea>
-    <button id="create-status">Publicar estado</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-8 mx-auto">
+                <div class="card border-0 bg-light">
+                    <form action="{{ route('statuses.store') }}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            <textarea name="body" class="form-control border-0 bg-light" placeholder="¿Qué estás pensando Jorge?"></textarea>
+                        </div>
+                        <div class="card-footer">
+                            <button id="create-status" class="btn btn-primary">Publicar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
