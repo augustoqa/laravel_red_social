2f60983530c04cbd4cfc0b53fdf4ffc987a717a2
César Acual <augustoacual@gmail.com>
2021-04-07 12:40:41 -0600

    Diseñando el formulario de login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light navbar-sodialapp">
         <div class="container">
-            <a class="navbar-brand" href="#">SocialApp</a>
+            <a class="navbar-brand" href="{{ route('home') }}">SocialApp</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -29,18 +29,23 @@
                 </ul>
 
                 <ul class="navbar-nav ml-auto">
+                    @guest
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+                    @else
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
+                            {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="#">Action</a>
                             <a class="dropdown-item" href="#">Another action</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a onclick="document.getElementById('logout').submit()" class="dropdown-item" href="#">Cerrar sesió<noscript></noscript></a>
                         </div>
                     </li>
+                    <form id="logout" action="{{ route('logout') }}" method="POST">@csrf</form>
+                    @endguest   
                 </ul>
             </div>
         </div>
