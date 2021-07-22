910adb711dc38f0206be3be2a251df9df23ff7cc
César Acual <augustoacual@gmail.com>
2021-07-22 11:22:50 -0600

    Recurso API para comentarios

---
+++ tests/Unit/Http/Resources/CommentResourceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Http\Resources;
+
+use App\Models\Status;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Http\Resources\CommentResource;
+
+class CommentResourceTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function a_comment_resources_must_have_the_necessary_fields()
+    {
+        $comment = factory(Status::class)->create();
+
+        $commentResource = CommentResource::make($comment)->resolve();
+
+        $this->assertEquals(
+            $comment->body, 
+            $commentResource['body']
+        );
+    }
+}
